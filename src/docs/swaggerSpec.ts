037907f561f061a4dc86cf349089c9f8607812de
aungkyawminhtet <aungkyawminhtet.sbo@gmail.com>
2026-05-22 21:50:10 +0700

    fix port

--- src/docs/swaggerSpec.ts
+++ src/docs/swaggerSpec.ts
@@ -11,7 +11,7 @@ const swaggerSpec = {
   },
   servers: [
     {
-      url: "http://localhost:3000",
+      url: "http://localhost:3001",
       description: "Local Development Server",
     },
     {
